e76555882fa73c4c7c14b88ab43e301e15434c38
Dayanch Muhammetgeldiyev <66455000+DayanchMuhammetgeldiyev@users.noreply.github.com>
2023-01-19 22:35:10 +0300

    update

---
+++ Rust-4-loop/target/debug/Rust-4-loop.d
@@ -0,0 +1,1 @@
+C:\Users\Dayanch\Documents\GitHub\Rust-learn\Rust-4-loop\target\debug\Rust-4-loop.exe: C:\Users\Dayanch\Documents\GitHub\Rust-learn\Rust-4-loop\src\main.rs
---
+++ Rust-4-loop/target/debug/deps/Rust_4_loop.d
@@ -0,0 +1,5 @@
+C:\Users\Dayanch\Documents\GitHub\Rust-learn\Rust-4-loop\target\debug\deps\Rust_4_loop.exe: src\main.rs
+
+C:\Users\Dayanch\Documents\GitHub\Rust-learn\Rust-4-loop\target\debug\deps\Rust_4_loop.d: src\main.rs
+
+src\main.rs:
