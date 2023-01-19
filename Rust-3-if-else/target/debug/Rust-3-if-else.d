0b73f5d8e37855cb90b4f12876c54ec878cdf989
Dayanch Muhammetgeldiyev <66455000+DayanchMuhammetgeldiyev@users.noreply.github.com>
2023-01-19 22:44:31 +0300

    Rust update

---
+++ Rust-3-if-else/target/debug/Rust-3-if-else.d
@@ -0,0 +1,1 @@
+C:\Users\Dayanch\Documents\GitHub\Rust-learn\Rust-3-if-else\target\debug\Rust-3-if-else.exe: C:\Users\Dayanch\Documents\GitHub\Rust-learn\Rust-3-if-else\src\main.rs
---
+++ Rust-3-if-else/target/debug/deps/Rust_3_if_else.d
@@ -0,0 +1,5 @@
+C:\Users\Dayanch\Documents\GitHub\Rust-learn\Rust-3-if-else\target\debug\deps\Rust_3_if_else.exe: src\main.rs
+
+C:\Users\Dayanch\Documents\GitHub\Rust-learn\Rust-3-if-else\target\debug\deps\Rust_3_if_else.d: src\main.rs
+
+src\main.rs:
