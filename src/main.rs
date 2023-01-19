229a7984204d39e77a1b435b1cc20ad730e3907e
Dayanch Muhammetgeldiyev <66455000+DayanchMuhammetgeldiyev@users.noreply.github.com>
2023-01-19 22:06:08 +0300

    Rust update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use std::io;
+// use std::io;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {guess}");
+    // println!("Guess the number!");
+    // println!("Please input your guess.");
+    // let mut guess = String::new();
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read line");
+    // println!("You guessed: {guess}");
+
+        //  İf Else Statements
+    // let n = 10 ;
+    // if n < 30 {
+    //     println!("This number is less than 30!")
+    // } else   {
+    //     println!("True")
+    // }
 }
